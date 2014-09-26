305c80a32ab643dd1397b724c88d4c68527464c7
ahok64554 <ashoksahu.baba@gmail.com>
2014-09-26 20:06:22 +0530

    asda
    dsad

--- Azure/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('vendor\autoloader.php');
-use WindowsAzure\Common\ServicesBuilder;
-use WindowsAzure\Common\ServiceException;
-$connectionString = "UseDevelopmentStorage=true";
-$tableRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
-?>
